55b4c1c78ced13f056c1da296dda04943e119340
daolang.huang <daolang.huang@thoughtworks.com>
2020-06-16 22:14:44 +0800

    refactor: remove unused import. Yujia&Daolang

--- src/main/java/com/thoughtworks/Locker.java
+++ src/main/java/com/thoughtworks/Locker.java
@@ -1,10 +1,6 @@
 package com.thoughtworks;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Locker {
