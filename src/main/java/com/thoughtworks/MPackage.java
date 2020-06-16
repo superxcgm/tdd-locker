5a40549736906f4d556340c98563d5b2805197e8
daolang.huang <daolang.huang@thoughtworks.com>
2020-06-16 20:45:49 +0800

    feta: should_take_the_correct_package_when_take_package_given_ticket_valid. Yujia&Daolang

--- src/main/java/com/thoughtworks/MPackage.java
+++ src/main/java/com/thoughtworks/MPackage.java
@@ -1,9 +1,23 @@
 package com.thoughtworks;
 
-import com.sun.tools.corba.se.idl.constExpr.Equal;
-
-import java.util.UUID;
+import java.util.Objects;
 
 public class MPackage {
+    String name;
+    MPackage(String name) {
+        this.name = name;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MPackage mPackage = (MPackage) o;
+        return Objects.equals(name, mPackage.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
--- src/test/java/com/thoughtworks/LockerTest.java
+++ src/test/java/com/thoughtworks/LockerTest.java
@@ -9,7 +9,7 @@ public class LockerTest {
     void should_return_ticket_when_store_package_given_locker_not_full() {
         Locker locker = new Locker(1);
 
-        Ticket ticket = locker.storePackage(new MPackage());
+        Ticket ticket = locker.storePackage(new MPackage(""));
 
         Assertions.assertNotNull(ticket);
     }
@@ -17,20 +17,32 @@ public class LockerTest {
     @Test
     void should_throw_exception_when_store_package_given_locker_full() {
         Locker locker = new Locker(1);
-        locker.storePackage(new MPackage());
+        locker.storePackage(new MPackage(""));
 
-        Assertions.assertThrows(LockerFullException.class, () -> locker.storePackage(new MPackage()));
+        Assertions.assertThrows(LockerFullException.class, () -> locker.storePackage(new MPackage("")));
     }
 
     @Test
     void should_take_package_success_when_take_package_given_ticket_valid() {
         Locker locker = new Locker(1);
-        MPackage mPackage = new MPackage();
+        MPackage mPackage = new MPackage("");
         Ticket ticket = locker.storePackage(mPackage);
 
         MPackage gotPackage = locker.takePackage(ticket);
 
-        Assertions.assertTrue(mPackage == gotPackage);
+        Assertions.assertTrue(mPackage.equals(gotPackage));
+    }
+
+    @Test
+    void should_take_the_correct_package_when_take_package_given_ticket_valid() {
+        Locker locker = new Locker(2);
+        MPackage onePackage = new MPackage("123");
+        MPackage twoPackage = new MPackage("456");
+
+        Ticket oneTicket = locker.storePackage(onePackage);
+        MPackage gotPackage = locker.takePackage(oneTicket);
+
+        Assertions.assertFalse(gotPackage.equals(twoPackage));
     }
 
     @Test
@@ -43,7 +55,7 @@ public class LockerTest {
     @Test
     void should_throw_exception_when_take_package_given_used_ticket() {
         Locker locker = new Locker(1);
-        Ticket ticket = locker.storePackage(new MPackage());
+        Ticket ticket = locker.storePackage(new MPackage(""));
         locker.takePackage(ticket);
 
         Assertions.assertThrows(TicketInvalidException.class, () -> locker.takePackage(ticket));
