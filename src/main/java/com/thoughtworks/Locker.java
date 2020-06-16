4df22cea3e571129a4a2359c1026df0643505fd7
daolang.huang <daolang.huang@thoughtworks.com>
2020-06-16 20:35:00 +0800

    refactor: store MPackage, take MPackage. Yujia&Daolang

--- src/main/java/com/thoughtworks/Locker.java
+++ src/main/java/com/thoughtworks/Locker.java
@@ -1,29 +1,31 @@
 package com.thoughtworks;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Locker {
     private int capacity;
-    private List<Ticket> list = new ArrayList<>();
-
+    private Map<Ticket, MPackage> items = new HashMap<>();
     public Locker(int capacity) {
         this.capacity = capacity;
     }
 
-    public Ticket storePackage() {
-        if (list.size() >= capacity) {
+    public Ticket storePackage(MPackage mPackage) {
+        if (items.size() >= capacity) {
             throw new LockerFullException();
         }
         Ticket ticket = Ticket.getNextTick();
-        list.add(ticket);
+        items.put(ticket, mPackage);
         return ticket;
     }
 
-    public void takePackage(Ticket ticket) {
-        if (list.contains(ticket)) {
-            list.remove(ticket);
-            return;
+    public MPackage takePackage(Ticket ticket) {
+        if (items.containsKey(ticket)) {
+            MPackage mPackage = items.get(ticket);
+            items.remove(ticket);
+            return mPackage;
         }
         throw new TicketInvalidException();
     }
--- src/main/java/com/thoughtworks/MPackage.java
+++ src/main/java/com/thoughtworks/MPackage.java
@@ -1,4 +1,9 @@
 package com.thoughtworks;
 
+import com.sun.tools.corba.se.idl.constExpr.Equal;
+
+import java.util.UUID;
+
 public class MPackage {
+
 }
