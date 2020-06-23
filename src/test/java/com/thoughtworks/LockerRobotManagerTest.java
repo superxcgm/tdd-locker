d07208934585495ebade7afb918f6ac5d2363d8c
daolang.huang <daolang.huang@thoughtworks.com>
2020-06-23 21:10:27 +0800

    should_throw_LockerFullException_when_store_bag_given_robot_manager_has_one_robot_has_one_locker_all_full. Yujia&Daolang

--- src/test/java/com/thoughtworks/LockerRobotManagerTest.java
+++ src/test/java/com/thoughtworks/LockerRobotManagerTest.java
@@ -121,4 +121,18 @@ public class LockerRobotManagerTest {
 
         Assertions.assertTrue(lockers1.get(0).isReleasedTicket(ticket));
     }
+
+    @Test
+    void should_throw_LockerFullException_when_store_bag_given_robot_manager_has_one_robot_has_one_locker_all_full() {
+        List<Locker> lockers = Arrays.asList(new Locker(1));
+        List<Locker> lockers1 = Arrays.asList(new Locker(1));
+        PrimaryLockerRobot robot1 = new PrimaryLockerRobot(lockers);
+        LockerRobotManager robotManager = new LockerRobotManager(Arrays.asList(robot1), lockers1);
+        robotManager.storeBag(new Bag(""));
+        robotManager.storeBag(new Bag(""));
+
+        Assertions.assertThrows(LockerFullException.class, () -> {
+            robotManager.storeBag(new Bag(""));
+        });
+    }
 }
